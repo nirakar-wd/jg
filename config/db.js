56e291d639c9204bdaedc4d14b5ec268145d9547
nirakar <niraztandukar@gmail.com>
2024-09-06 15:33:09 +0545

    changed mysql port to run in local port

--- config/db.js
+++ config/db.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 var mysql = require("mysql");
 
 module.exports = {
-  HOST: process.env.DB_HOST || "localhost",
-  PORT: process.env.DB_PORT || "3308",
-  USER: process.env.DB_USER || "root",
-  PASSWORD: process.env.DB_PASSWORD || "",
-  DB: process.env.DB_NAME || "jg_ecommerce",
-  dialect: "mysql",
+    HOST: process.env.DB_HOST || "localhost",
+    PORT: process.env.DB_PORT || "3306",
+    USER: process.env.DB_USER || "root",
+    PASSWORD: process.env.DB_PASSWORD || "",
+    DB: process.env.DB_NAME || "jg_ecommerce",
+    dialect: "mysql",
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
 };
